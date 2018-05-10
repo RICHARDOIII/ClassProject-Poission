b8fc56774413f9a3267261ee8c4cd06bddb68158
Daniel Espinoza <danieler@login2.maverick.tacc.utexas.edu>
2018-05-09 19:56:57 -0500

    gs

--- Poission.c
+++ Poission.c
@@ -1,58 +1,112 @@
-#include <stdlib>
-#include <cmath>
-#include <iostream>
-void MULTIGRID_CYCLE(float &ERROR, int levels, float array)
-{	levels=levels-1;
-	float LError=0;
-	GAUSS(LError,
-	float pass
-}
-void GAUSS(float &LERROR,float *passarray)
 
-void restriction(
+#include <stdlib.h>
+#include <math.h>
+#include <iostream>
+#include <stdio.h>
+void gs(float &WE,float *array, int dis,int max,int st)
+			{ float compare = array[st*max+st];
+                	for(int i=st;i<max-st;i=i+dis)
+                	{
+                		for (int j=st;j<max-st;j=j+dis)
+                		{
+                			array[i*max+j] = .25(array[(i+dis)*max+j]
+							+array[(i-dis)*max+j]
+							+array[i*max+(j+dis)]
+							+array[i*max+(j-dis)]);
+                		}
+                	}
+                	WE=abs((compare-array[st*max+st])/compare);
+			}
+
+//void restriction(
+////void prolong(
+ void MULTIGRID_CYCLE(float &WE, int &level,int maxl, float *array)
+//  	{
+	int max = pow(2,maxl)+1;
+	int dis = pow(2,level);
+	int st=level*2;
+	gs(WE,array,dis,max,st);
+	RESTRICT()
+	MULTIGRID_CYCLE(WE,level,maxl,array);
+	}
 
 int main()
-{	
-
-	// Size of the rows/columns array that will be pass through	
-	int n = 9 //Undetermined amount
-	int *array=(int *)malloc( n* n * sizeof(float));
-	
-
-//	start partioning the array in courser and courser grids
-//	Operates on a (2^i+1)by(2^i+1)grid
-//	With a (2^i-1) by (2^i-1) grid
-//      EXAMPLE
-//      /	/	/
-//      /	U	/	The  Grid starts from the Center and Goes out so we want to 
-//      /	/	/	make the thing recursive
-		//	
-	int numberofgrids=0;
-	for (int i=0;i++)
+{
+ int n = 9; //Undetermined amount
+float *array=(float *)malloc( n* n * sizeof(int));
+int numberofgrids=0;
+for (int i=0;i<n;i++)
+{
+	if (n-(pow(2,i)+1)==0)
 	{
-		numberofgrids+=1;
-		if (n-(pow(2,i)+1)==0)
-			{
-			break;
-			}
+		break;
 	}
-	
-	while error<.001
-	{	int l=numberofgrids;
-		for(int i=0;i<numberofgrids;
-		{	
-			int Lerror=0;
-				for (int j=0;j<l;j++)
-				{	
-	// MULTIGRIDCYCLE
-	// STEPS:1
-	// SMOOTH
-	// 2: RESTRICT
-	// 3:GO DOWN A LEVEL 
-	// 4:IF level is 0 then do things
-	
-// START WITH MAX ERROR
+	numberofgrids=numberofgrids+1;
 }
+	printf("%d\n",numberofgrids);
+	float WE=1;
+	int level=0;
+	MULTIGRID_CYCLE(WE,level,numberofgrids-1,array);
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
